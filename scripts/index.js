df18f298a890cd2665323d635da490f8c101899c
Aleksandra Sidorova <mehaleka240399@yandex.ru>
2025-06-02 21:34:21 +0300

    fix: доработана функция добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -10,17 +10,46 @@
 
 
 // добавлен слушатель на кнопку с плюсиком
-const buttonAdd = document.querySelector('.profile__add-button')
+const buttonAdd = document.querySelector('.profile__add-button');
 
 buttonAdd.addEventListener('click', function (evt) {
   console.log('нажалась кнопка с плюсиком', evt.type);
 });
 
+
 // место, где должны быть карточки
 const placesList = document.querySelector('.places__list');
+
+// функция для ОДНОЙ карточки 
+function uploadingOneCard (initialCard) {
+
+  const li = document.createElement('li');
+  li.classList.add('places__item', 'card');
+  // создание карточки
+  li.innerHTML = `
+    <img class="card__image" src="${initialCard.link}" alt="" />
+    <button type="button" class="card__delete-button"></button>
+    <div class="card__description">
+      <h2 class="card__title">
+      ${initialCard.name}
+      </h2>
+      <button type="button" class="card__like-button"></button>
+    </div>`
+  return li;
+};
+
+// функция для вывода карточек
+function appendCards (initialCard, placesList) {
+  placesList.append(initialCard);
+};
+
 // функция для КАРТОЧЕК и КУДА
-function uploadingCards (initialCards, container) {
-  console.log(initialCards);
+function uploadingCards (initialCards, placesList) {
+  // перебираем ВСЕ карточки
+  initialCards.forEach(initialCard => {
+    const card = uploadingOneCard (initialCard);
+    // вызываем функцию ВЫВОДА КАРТОЧЕК
+    appendCards(card, placesList);
+  });
 };
 uploadingCards(initialCards, placesList);
-
