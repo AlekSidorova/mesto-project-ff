9b88cc64a259cfaab4fb88d2c5c58d3ef9192fd0
Aleksandra Sidorova <mehaleka240399@yandex.ru>
2025-06-02 20:44:16 +0300

    feat: добавлен слушатель на кнопку

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,21 @@
 
 // @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+// @todo: Вывести карточки на страницу\
+
+
+// добавлен слушатель на кнопку с плюсиком
+const buttonAdd = document.querySelector('.profile__add-button')
+
+buttonAdd.addEventListener('click', function (evt) {
+  console.log('нажалась кнопка с плюсиком', evt.type);
+});
+
+// место, где должны быть карточки
+const placesList = document.querySelector('.places__list');
+// функция для КАРТОЧЕК и КУДА
+function uploadingCards (initialCards, container) {
+  console.log(initialCards);
+};
+uploadingCards(initialCards, placesList);
+
