2d42c3ec5fab7287041b39d60b14a6c294be08cf
Aleksandra Sidorova <mehaleka240399@yandex.ru>
2025-06-03 17:05:22 +0300

    feat:добавлена функция удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,18 @@ const placesList = document.querySelector('.places__list');
 // находим сам шаблон
 const cardTemplate = document.querySelector('#card-template');
 
+// функция для удаления карточки
+function deleteCard(evt) {
+  // находим ближайшего родителя
+  const cardDelete = evt.target.closest('.card');
+  if (cardDelete) {
+    // удаляем карточку
+    cardDelete.remove();
+  }
+};
+
 // создаем функцию для одной карточки
-function card(initialCards){
+function createCard(initialCards){
   // создаем одну карточку и клонируем ее
   const initialCard = cardTemplate.content.cloneNode(true);
 
@@ -13,13 +23,21 @@ function card(initialCards){
   const imageCard = initialCard.querySelector('.card__image');
   const titleCard = initialCard.querySelector('.card__title');
 
+  // находим кнопку удаления
+  const deleteButton = initialCard.querySelector('.card__delete-button');
+
   // меняем данные
   imageCard.src = initialCards.link; 
   titleCard.textContent = initialCards.name;
 
-  // Добавляем карточку в общий список
+  // добавляем обработчик на кнопку удаления
+  deleteButton.addEventListener('click', deleteCard);
+
+  // ставим карточку в общий список
   placesList.append(initialCard);
 };
 
-// перебираем весь массив 
-initialCards.forEach(card);
+// перебираем весь массив и выводим все карточки
+initialCards.forEach(createCard);
+
+
