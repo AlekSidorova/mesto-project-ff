f742cd4c0cf35921aebfff684a9b5529bd7452f5
Aleksandra Sidorova <mehaleka240399@yandex.ru>
2025-06-03 17:31:12 +0300

    refactor: доработка и чистка кода

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,41 @@
 // место, куда эти карточки класть
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector(".places__list");
 
 // находим сам шаблон
-const cardTemplate = document.querySelector('#card-template');
+const cardTemplate = document.querySelector("#card-template");
 
 // функция для удаления карточки
 function deleteCard(evt) {
   // находим ближайшего родителя
-  const cardDelete = evt.target.closest('.card');
+  const cardDelete = evt.target.closest(".card");
   if (cardDelete) {
     // удаляем карточку
     cardDelete.remove();
   }
-};
+}
 
 // создаем функцию для одной карточки
-function createCard(initialCards){
+function createCard(initialCards) {
   // создаем одну карточку и клонируем ее
   const initialCard = cardTemplate.content.cloneNode(true);
 
   // находим данные, которые нужно поменять
-  const imageCard = initialCard.querySelector('.card__image');
-  const titleCard = initialCard.querySelector('.card__title');
-
-  // находим кнопку удаления
-  const deleteButton = initialCard.querySelector('.card__delete-button');
+  const imageCard = initialCard.querySelector(".card__image");
+  const titleCard = initialCard.querySelector(".card__title");
 
   // меняем данные
-  imageCard.src = initialCards.link; 
+  imageCard.src = initialCards.link;
   titleCard.textContent = initialCards.name;
 
+  // находим кнопку удаления
+  const deleteButton = initialCard.querySelector(".card__delete-button");
+
   // добавляем обработчик на кнопку удаления
-  deleteButton.addEventListener('click', deleteCard);
+  deleteButton.addEventListener("click", deleteCard);
 
   // ставим карточку в общий список
   placesList.append(initialCard);
-};
+}
 
 // перебираем весь массив и выводим все карточки
 initialCards.forEach(createCard);
-
-
