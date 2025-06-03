29625ab9378de7d4c3d88a85db1051bb6bb89d7e
Aleksandra Sidorova <mehaleka240399@yandex.ru>
2025-06-03 16:11:50 +0300

    refactor: добавлены карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,75 +1,25 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу\
-
-
-// добавлен слушатель на кнопку с плюсиком
-const buttonAdd = document.querySelector('.profile__add-button');
-
-buttonAdd.addEventListener('click', function (evt) {
-  console.log('нажалась кнопка с плюсиком', evt.type);
-});
-
-
-// место, где должны быть карточки
+// место, куда эти карточки класть
 const placesList = document.querySelector('.places__list');
 
-// находим шаблон
+// находим сам шаблон
 const cardTemplate = document.querySelector('#card-template');
 
-// перебираем ВЕСЬ массив
-initialCards.forEach(function (initialCards) {
-  // клонируем содержимое шаблона
-  const cardItem = cardTemplate.content.cloneNode(true);
-  // Заполняем данные
-  const imageCard = cardItem.querySelector('.card__image');
-  const titleCard = cardItem.querySelector('.card__title');
- // меняем данные
+// создаем функцию для одной карточки
+function card(initialCards){
+  // создаем одну карточку и клонируем ее
+  const initialCard = cardTemplate.content.cloneNode(true);
+
+  // находим данные, которые нужно поменять
+  const imageCard = initialCard.querySelector('.card__image');
+  const titleCard = initialCard.querySelector('.card__title');
+
+  // меняем данные
   imageCard.src = initialCards.link; 
-  titleCard.textContent = initialCards.name; 
+  titleCard.textContent = initialCards.name;
 
   // Добавляем карточку в общий список
-  placesList.appendChild(cardItem);
-  });
-
-
-/* // функция для ОДНОЙ карточки 
-function uploadingOneCard (initialCard) {
-
-  const li = document.createElement('li');
-  li.classList.add('places__item', 'card');
-  // создание карточки
-  li.innerHTML = `
-    <img class="card__image" src="${initialCard.link}" alt="" />
-    <button type="button" class="card__delete-button"></button>
-    <div class="card__description">
-      <h2 class="card__title">
-      ${initialCard.name}
-      </h2>
-      <button type="button" class="card__like-button"></button>
-    </div>`
-  return li;
-};
-
-// функция для вывода карточек
-function appendCards (initialCard, placesList) {
   placesList.append(initialCard);
-}; 
-
-// функция для КАРТОЧЕК и КУДА
-function uploadingCards (initialCards, placesList) {
-  // перебираем ВСЕ карточки
-  initialCards.forEach(initialCard => {
-    const card = uploadingOneCard (initialCard);
-    // вызываем функцию ВЫВОДА КАРТОЧЕК
-    appendCards(card, placesList);
-  });
 };
-uploadingCards(initialCards, placesList); 
- */
+
+// перебираем весь массив 
+initialCards.forEach(card);
