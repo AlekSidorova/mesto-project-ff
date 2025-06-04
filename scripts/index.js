172f1d42f692f33e496ee60e32e360b1882726c9
Aleksandra Sidorova <mehaleka240399@yandex.ru>
2025-06-04 14:44:40 +0300

    fix: исправление ошибок после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -6,36 +6,35 @@ const cardTemplate = document.querySelector("#card-template");
 
 // функция для удаления карточки
 function deleteCard(evt) {
-  // находим ближайшего родителя
   const cardDelete = evt.target.closest(".card");
   if (cardDelete) {
-    // удаляем карточку
     cardDelete.remove();
   }
 }
 
 // создаем функцию для одной карточки
-function createCard(initialCards) {
+function createCard(initialCards, deleteCard) {
   // создаем одну карточку и клонируем ее
   const initialCard = cardTemplate.content.cloneNode(true);
 
-  // находим данные, которые нужно поменять
+  // находим данные и меняем
   const imageCard = initialCard.querySelector(".card__image");
   const titleCard = initialCard.querySelector(".card__title");
-
-  // меняем данные
   imageCard.src = initialCards.link;
+  imageCard.alt = initialCards.name;
   titleCard.textContent = initialCards.name;
 
-  // находим кнопку удаления
   const deleteButton = initialCard.querySelector(".card__delete-button");
-
-  // добавляем обработчик на кнопку удаления
   deleteButton.addEventListener("click", deleteCard);
 
-  // ставим карточку в общий список
-  placesList.append(initialCard);
+
+  // возвращаем созданную одну карточку
+  return initialCard;
 }
 
 // перебираем весь массив и выводим все карточки
-initialCards.forEach(createCard);
+initialCards.forEach(card => {
+  const cardElement = createCard(card, deleteCard);
+  placesList.append(cardElement);
+});
+
